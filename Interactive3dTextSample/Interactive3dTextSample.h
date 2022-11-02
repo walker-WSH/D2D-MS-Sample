e43f5e9de84408f6c70e7b4329e9101a4928c75d
wang.shaohui <wang.shaohui@navercorp.com>
2022-11-02 16:42:09 +0800

    save

--- Interactive3dTextSample/Interactive3dTextSample.h
+++ Interactive3dTextSample/Interactive3dTextSample.h
@@ -28,7 +28,6 @@
 #include <d2d1.h>
 #include <d2d1helper.h>
 #include <dwrite.h>
-#include <wincodec.h>
 
 /******************************************************************
 *                                                                 *
--- MSAARenderingSample/MSAARenderingSample.h
+++ MSAARenderingSample/MSAARenderingSample.h
@@ -26,7 +26,6 @@
 #include <d2d1.h>
 #include <d2d1helper.h>
 #include <dwrite.h>
-#include <wincodec.h>
 
 #include <strsafe.h>
 
